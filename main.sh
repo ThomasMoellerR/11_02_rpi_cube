3c97d0e1809df6f48fdb77021cf1ca43a73d2c5e
test <test>
2019-05-16 15:40:36 +0200

    test

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 sudo python3 "/home/pi/a/cube/main.py" \
---mqtt_server_ip "192.168.2.52" \
+--mqtt_server_ip "192.168.178.52" \
 --mqtt_server_port "1883" \
 --mqtt_topic_set_brightness "cube/brightness" \
 --mqtt_topic_set_mode "cube/mode" \
